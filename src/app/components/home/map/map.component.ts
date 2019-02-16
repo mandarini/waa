5301ae5973551e7586008acdb48b91b188cc3ce5
katerina <sk.katherine@gmail.com>
2019-02-16 20:03:47 +0200

    some css

--- src/app/components/home/map/map.component.ts
+++ src/app/components/home/map/map.component.ts
@@ -11,7 +11,6 @@ import { Artist } from "src/app/objects/artist";
 import { ArtistDetails } from "src/app/objects/artist-details";
 import { Space } from "src/app/objects/space";
 import { ScriptLoadService } from "src/app/services/script-load.service";
-import { InfoMap } from "src/app/objects/info-map";
 
 const your_API_key = "AIzaSyBV4CbNglZMZTc9Qnh2iTTZvL8c0eVtHw0";
 const url = `https://maps.googleapis.com/maps/api/js?key=${your_API_key}&libraries=geometry`;
